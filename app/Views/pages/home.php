17e57d40e52611d97d3318a5cf8b53f4e7ff025d
Manuel <mrt00033@red.ujaen.es>
2023-03-22 16:18:07 +0100

    Animaciones para los dropdown y el menú lateral

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -212,9 +212,10 @@
         </li><!-- End Register Page Nav -->
 
         <li class="nav-item">
-            <a class="nav-link collapsed" href="pages-login.html">
+            <a class="nav-link collapsed" href="/login">
                 <i class="bi bi-box-arrow-in-right"></i>
                 <span>Login</span>
+                
             </a>
         </li><!-- End Login Page Nav -->
 
--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -22,7 +22,7 @@
 <script src="js/validate.js"></script>
 
 <!-- Template Main JS File -->
-<script src="assets/js/main.js"></script>
+<script src="js/main.js"></script>
 
 </body>
 
