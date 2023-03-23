2ffb8c3bd34e43a589b1d07c671b5674e983982e
Manuel <mrt00033@red.ujaen.es>
2023-03-23 17:46:10 +0100

    Arreglo de las imagenes y limpieza de la interfaz

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -18,4 +18,17 @@
 
 </body>
 
+<footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Manolo y JC</span></strong>. All Rights Reserved & Not Copyright intended
+    </div>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+      Designed by Manolo y JC
+    </div>
+  </footer><!-- End Footer -->
+
 </html>
